9d9073af70c4c1dee068b1be19e7dea04b5e13a6
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2023-03-22 20:30:40 -0300

    Updates

--- dist/react/index.d.ts
+++ dist/react/index.d.ts
@@ -1,4 +1,4 @@
 export { default as CpsButton } from './button';
-export { default as CpsInclude } from './include';
 export { default as CpsIcon } from './icon';
 export { default as CpsSpinner } from './spinner';
+export { default as CpsInclude } from './include';
