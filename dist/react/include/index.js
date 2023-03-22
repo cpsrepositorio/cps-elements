5ad3f0eb62c355b6190fa8624e684270c02ee150
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2023-03-22 20:47:17 -0300

    Updates

--- dist/react/include/index.js
+++ dist/react/include/index.js
@@ -1,11 +1,11 @@
 import {
   include_default
-} from "../../chunks/chunk.VDMMHTTB.js";
-import "../../chunks/chunk.UACVX4U3.js";
+} from "../../chunks/chunk.5VRUCU74.js";
+import "../../chunks/chunk.VN4657OP.js";
 import "../../chunks/chunk.K3DUMMUS.js";
 import "../../chunks/chunk.C7FWPEOY.js";
-import "../../chunks/chunk.KLFQZSBV.js";
 import "../../chunks/chunk.RPB53XXV.js";
+import "../../chunks/chunk.KLFQZSBV.js";
 import "../../chunks/chunk.JQQCW4UB.js";
 import "../../chunks/chunk.6M63UXML.js";
 export {
