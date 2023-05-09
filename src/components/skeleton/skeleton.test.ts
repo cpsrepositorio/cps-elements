cfe2842f2bf1730d3c9fc8852689562be876e50a
Erick Eduardo Petrucelli <erickpetru@gmail.com>
2023-05-09 13:10:16 -0300

    tests: fix skeleton tests

--- src/components/skeleton/skeleton.test.ts
+++ src/components/skeleton/skeleton.test.ts
@@ -10,8 +10,8 @@ describe('<cps-skeleton>', () => {
     const base = el.shadowRoot!.querySelector<HTMLElement>('[part~="base"]')!;
     const indicator = el.shadowRoot!.querySelector<HTMLElement>('[part~="indicator"]')!;
 
-    expect(base.getAttribute('class')).to.equal(' skeleton ');
-    expect(indicator.getAttribute('class')).to.equal('skeleton__indicator');
+    expect(base.getAttribute('class')?.trim()).to.equal('skeleton skeleton--rounded skeleton--sheen');
+    expect(indicator.getAttribute('class')?.trim()).to.equal('skeleton__indicator');
   });
 
   it('should set pulse effect by attribute', async () => {
@@ -19,14 +19,38 @@ describe('<cps-skeleton>', () => {
 
     const base = el.shadowRoot!.querySelector<HTMLElement>('[part~="base"]')!;
 
-    expect(base.getAttribute('class')).to.equal(' skeleton skeleton--pulse ');
+    expect(base.getAttribute('class')?.trim()).to.equal('skeleton skeleton--rounded skeleton--pulse');
   });
 
-  it('should set sheen effect by attribute', async () => {
-    const el = await fixture<CpsSkeleton>(html` <cps-skeleton effect="sheen"></cps-skeleton> `);
+  it('should set none effect by attribute', async () => {
+    const el = await fixture<CpsSkeleton>(html` <cps-skeleton effect="none"></cps-skeleton> `);
 
     const base = el.shadowRoot!.querySelector<HTMLElement>('[part~="base"]')!;
 
-    expect(base.getAttribute('class')).to.equal(' skeleton skeleton--sheen ');
+    expect(base.getAttribute('class')?.trim()).to.equal('skeleton skeleton--rounded');
+  });
+
+  it('should set corner rounded by attribute', async () => {
+    const el = await fixture<CpsSkeleton>(html` <cps-skeleton rounded="corner"></cps-skeleton> `);
+
+    const base = el.shadowRoot!.querySelector<HTMLElement>('[part~="base"]')!;
+
+    expect(base.getAttribute('class')?.trim()).to.equal('skeleton skeleton--rounded-corner skeleton--sheen');
+  });
+
+  it('should set full rounded by attribute', async () => {
+    const el = await fixture<CpsSkeleton>(html` <cps-skeleton rounded="full"></cps-skeleton> `);
+
+    const base = el.shadowRoot!.querySelector<HTMLElement>('[part~="base"]')!;
+
+    expect(base.getAttribute('class')?.trim()).to.equal('skeleton skeleton--rounded-full skeleton--sheen');
+  });
+
+  it('should set none rounded by attribute', async () => {
+    const el = await fixture<CpsSkeleton>(html` <cps-skeleton rounded="none"></cps-skeleton> `);
+
+    const base = el.shadowRoot!.querySelector<HTMLElement>('[part~="base"]')!;
+
+    expect(base.getAttribute('class')?.trim()).to.equal('skeleton skeleton--sheen');
   });
 });
