3d53e44c0e689fdc2608dabbe40f6238c05f987d
Erick Eduardo Petrucelli <erickpetru@gmail.com>
2023-06-05 18:57:25 -0300

    docs(include): fix embedded permanent link

--- src/components/include.ts
+++ src/components/include.ts
@@ -8,7 +8,7 @@ import type { CSSResultGroup } from 'lit';
 
 /**
  * @summary Possibilita a inclusão de arquivos HTML externos diretamente no conteúdo da página.
- * @documentation https://cpsrepositorio.github.io/cps-elements/#/componentes/include
+ * @documentation https://cpsrepositorio.github.io/cps-elements/#/utilitários/include
  * @status stable
  * @since 0.1
  *
